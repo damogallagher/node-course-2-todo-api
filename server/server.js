285709de8843f3341fa09d1be18a0e98f9ea6778
Damien Gallagher <damien.gallagher@gmail.com>
2018-09-19 18:06:43 +0100

    Tests for the todos model

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
-    todo.save().then((doc)=> {
-        res.send(doc);
+    todo.save().then((doc)=> {  
+         res.send(doc);
     }, (e)=> {
         res.status(400).send(e);
     });
@@ -24,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = { app }
