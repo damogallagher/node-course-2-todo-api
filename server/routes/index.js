465b8aa18ab3077eedeccbcb2d3ca4d13db1f851
Damien Gallagher <damien.gallagher@gmail.com>
2018-09-21 23:47:49 +0100

    Added links to tutorials that were followed

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 var router = require('express').Router();
 
+//See this article for a description as to why the routes are split out
+//http://catlau.co/how-to-modularize-routes-with-the-express-router/
+
 router.use('/todos', require('./todos'));
 router.use('/users', require('./users'));
 
